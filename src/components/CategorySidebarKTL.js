7c2dcd53bc3f170e7d4eb91d549539c27a9fb34e
Kevin Luo <kvn.luo@gmail.com>
2021-03-23 15:15:04 -0400

    test importing json data in CategorySidebarKTL component

--- src/components/CategorySidebarKTL.js
+++ src/components/CategorySidebarKTL.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import categories from "../data/demo.json";
 
 export default function CategorySidebarKTL() {
   return (
     <div>
       Category Sidebar
+      {categories.categories.competitors[0]}
     </div>
   )
 }
