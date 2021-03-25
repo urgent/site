b414991bba98486f6752c87ddbb24692e72caf10
JD Martinez <focus4ursoul@gmail.com>
2021-03-25 14:06:24 -0700

    messaged to display with tags

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,9 +1,17 @@
 import data from "../data/demo.json";
+import Message from "./Messsage";
 
-export default function Messages(messages) {
+export default function Messages({ messages }) {
+
+    // console.log(messages[0].tags);
+
+    const displayMessages = messages.map((mssg, index) => {
+        return <Message key={index} body={mssg.body} tagPayload={mssg.tags} />
+    })
+
     return (
         <section className="mssgBoardWrapper">
-            {`Messages Display: ${data.messages[0].body}`}
+            {displayMessages}
         </section>
     )
 }
