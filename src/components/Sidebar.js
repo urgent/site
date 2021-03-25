bcea42410f22f237f50cf8ddac2de7f8dd383673
JD Martinez <focus4ursoul@gmail.com>
2021-03-25 11:46:38 -0700

    add/remove tags from global state -- no filter by category

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,7 @@
 import CategoryBox from "./CategoryBox";
-import { useSmoomsContext } from "../utils/SmoomsState";
 
 export default function Sidebar({ categories }) {
     const category = categories.map((cat, index) => <CategoryBox key={index} payload={cat} />)
-    const [{ value }, dispatch] = useSmoomsContext();
-
-    console.log(value);
 
     return (
         <section className="sideBarWrapper innerBorder">
