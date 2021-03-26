91e1299405fe2ed9161eb39973a600e988099dcf
JD Martinez <focus4ursoul@gmail.com>
2021-03-25 18:04:56 -0700

    testing set up for message filtering

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,9 +1,17 @@
 import data from "../data/demo.json";
 import Message from "./Messsage";
+import { useSmoomsContext } from "../utils/SmoomsState";
+
 
 export default function Messages({ messages }) {
 
-    // console.log(messages[0].tags);
+    const [{ activeTags }, dispatch] = useSmoomsContext();
+
+    console.log(activeTags, messages);
+
+    // const filteredMssgs = messages.filter(mssg => {
+
+    // })
 
     const displayMessages = messages.map((mssg, index) => {
         return <Message key={index} body={mssg.body} tagPayload={mssg.tags} />
--- src/components/Messsage.js
+++ src/components/Messsage.js
@@ -3,9 +3,10 @@ import Tag from "./Tag";
 
 export default function Messsage({ body, tagPayload }) {
 
+
+
     const mssgTags = tagPayload.map((tag, index) => {
 
-        // console.log(tag.detail);
         return <Tag key={index} tag={tag.detail} />
     })
 
