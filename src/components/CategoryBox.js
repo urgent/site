76c9a1cc1f09cbafa513f322cc3baa76171491a0
Kevin Luo <kvn.luo@gmail.com>
2021-04-02 12:56:27 -0400

    display categories from aws database

--- src/components/CategoryBox.js
+++ src/components/CategoryBox.js
@@ -4,18 +4,14 @@ import Tag from "./Tag";
 // this component displays an individual category in the sidebar
 export default function CategoryBox({ payload }) {
 
-  const tagsArr = Object.values(payload)[0].map((tag, index) => <Tag key={index} tag={tag} />)
-
-  // console.log(tagsArr);
-
-  // console.log(payload);
-
+  // const tagsArr = Object.values(payload)[0].map((tag, index) => <Tag key={index} tag={tag} />)
+  
   return (
 
     <div className="catContainer">
-      <h2 className="catTitle">{Object.keys(payload)[0]}</h2>
+      <h2 className="catTitle">{payload.name}</h2>
       <div className="tagsWrapper">
-        {tagsArr}
+        {/* {tagsArr} */}
       </div>
     </div>
   )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import CategoryBox from "./CategoryBox";
 
 // this is the sidebar component that displays all the categories
 export default function Sidebar({ categories }) {
-    const category = categories.map((cat, index) => <CategoryBox key={index} payload={cat} />)
+    const category = categories.map(cat => <CategoryBox key={cat.id} payload={cat} />)
 
     return (
         <section className="sideBarWrapper innerBorder">
