74ebf675fcb652fcdd3299d084bbc75a8acde6ca
Kevin Luo <kvn.luo@gmail.com>
2021-04-02 12:14:54 -0400

    call aws graphql api to fetch categories

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -1,10 +1,25 @@
 import MessageBoard from "../components/MessageBoard";
 import Sidebar from "../components/Sidebar";
 import data from "../data/demo.json";
+import { API } from 'aws-amplify';
+import { listCategorys } from '../../graphql/queries'
 
-// this component pulls the data
+// this component pulls categories from AWS API
 export default function MainWrapper() {
-    const { categories, messages } = data;
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(() => {
+      fetchCategories();
+    }, [])
+
+    // fetch call to Graphql API
+    async function fetchCategories() {
+      const categoryData = await API.graphql({
+        query: listCategorys
+      });
+      setCategories(categoryData.data.listCategorys.items);
+    }
+    // const { categories, messages } = data;
     return (
 
         <div className="mainWrapper">
