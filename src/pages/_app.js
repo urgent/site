96fdd6cf01cdd22424c4f0ec0ebf10d3f4372628
Kevin Luo <kvn.luo@gmail.com>
2021-04-02 12:01:39 -0400

    add configureAmplify so that we can call the graphql API

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "../../styles/mainWrapper.css";
 import "../../styles/mssgBoard.css";
 import "../../styles/sidebar.css";
 import "../../styles/catBox.css";
+import '../../configureAmplify'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
