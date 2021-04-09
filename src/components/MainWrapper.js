4330a278e5932575ef518125c4b895f1baf1b098
Kevin Luo <kvn.luo@gmail.com>
2021-04-09 18:12:47 -0400

    update graphql statement depth

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -1,33 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import MessageBoard from "../components/MessageBoard";
 import Sidebar from "../components/Sidebar";
-import data from "../data/demo.json";
-import { API } from 'aws-amplify';
-import { listCategorys } from '../../graphql/queries'
+import { API } from "aws-amplify";
+import { listCategorys } from "../../graphql/queries";
 
 // this component pulls categories from AWS API
 export default function MainWrapper() {
   // set state to hold categories
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    // fetch categories upon component load
-    useEffect(() => {
-      fetchCategories();
-    }, [])
+  // fetch categories upon component load
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    // fetch call to Graphql API
-    async function fetchCategories() {
-      const categoryData = await API.graphql({
-        query: listCategorys
-      });
-      setCategories(categoryData.data.listCategorys.items);
-    }
-    // const { categories, messages } = data;
-    return (
+  // fetch call to Graphql API
+  async function fetchCategories() {
+    const categoryData = await API.graphql({
+      query: listCategorys,
+    });
+    console.log(categoryData);
+    setCategories(categoryData.data.listCategorys.items);
+  }
 
-        <div className="mainWrapper">
-            <Sidebar categories={categories} />
-            {/* <MessageBoard messages={messages} /> */}
-        </div>
-    )
+  return (
+    <div className="mainWrapper">
+      <Sidebar categories={categories} />
+      {/* <MessageBoard messages={messages} /> */}
+    </div>
+  );
 }
