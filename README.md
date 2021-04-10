2fc9cf5d87caf6dbe748cda1e80a3f61a9736c5a
Kevin Luo <kvn.luo@gmail.com>
2021-04-10 18:02:59 -0400

    created the filter functionality to search for multiple tags

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 next chrome extension app - mvp
+
+or search
+
+```
+import { useEffect } from 'react'
+
+useEffect(() => {
+  if (activeTags.length > 0) {
+      let newMssgs = [];
+      let mssgIDs = {};
+
+      messages.forEach(mssg => {
+          let tagsArr = mssg.tags.items
+          tagsArr.forEach(obj => {
+              if (activeTags.includes(obj.tagID) && !mssgIDs[mssg.id]) {
+                  newMssgs.push(mssg);
+                  mssgIDs[mssg.id] = true;
+                  return
+              }
+          });
+          return
+      });
+      dispatch({ type: "filterMssgs", payload: { messages: newMssgs } });
+  }
+}, [activeTags]);
+```
--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,45 +1,56 @@
-import data from "../data/demo.json";
 import { useEffect } from "react";
 import Message from "./Messsage";
 import { useSmoomsContext } from "../utils/SmoomsState";
-import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
 
 // this component is the container for displaying all individual messages
 export default function Messages({ messages }) {
+  const [{ activeTags, activeMssgs }, dispatch] = useSmoomsContext();
 
-    const [{ activeTags, activeMssgs }, dispatch] = useSmoomsContext();
+  useEffect(() => {
+    if (activeTags.length > 0) {
+      let newMssgs = [];
+      let activeTagIDs = {};
 
-    useEffect(() => {
-        if (activeTags.length > 0) {
-            let newMssgs = [];
-            let mssgIDs = {};
+      messages.forEach((mssg) => {
+        let count = 0;
+        let tagsArr = mssg.tags.items;
 
-            messages.forEach(mssg => {
-                let tagsArr = mssg.tags.items
-                tagsArr.forEach(obj => {
-                    if (activeTags.includes(obj.tagID) && !mssgIDs[mssg.id]) {
-                        newMssgs.push(mssg);
-                        mssgIDs[mssg.id] = true;
-                        return
-                    }
-                });
-                return
-            });
-            dispatch({ type: "filterMssgs", payload: { messages: newMssgs } });
+        activeTags.forEach((aTag) => {
+          activeTagIDs[aTag] = true;
+        });
+
+        tagsArr.forEach((obj) => {
+          if (activeTagIDs[obj.tagID]) {
+            count++;
+          }
+        });
+
+        if (count === activeTags.length) {
+          newMssgs.push(mssg);
         }
-    }, [activeTags]);
 
-    const displayMessages = activeMssgs.length > 0 ?
-        activeMssgs.map(mssg => {
-            return (<Message key={mssg.id} body={mssg.body} tagPayload={mssg.tags?.items} />)
-        })
-        : (<div>No messages</div>);
+        count = 0;
+        return;
+      });
 
-    console.log(activeMssgs);
+      dispatch({ type: "filterMssgs", payload: { messages: newMssgs } });
+    }
+  }, [activeTags]);
 
-    return (
-        <section className="mssgBoardWrapper">
-            {displayMessages}
-        </section>
-    )
+  const displayMessages =
+    activeMssgs.length > 0 ? (
+      activeMssgs.map((mssg) => {
+        return (
+          <Message
+            key={mssg.id}
+            body={mssg.body}
+            tagPayload={mssg.tags?.items}
+          />
+        );
+      })
+    ) : (
+      <div>No messages</div>
+    );
+
+  return <section className="mssgBoardWrapper">{displayMessages}</section>;
 }
