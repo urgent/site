8bdfdd1f59b698b33cf32b755c84d5d8b1b1ef6a
Kevin Luo <kvn.luo@gmail.com>
2021-04-11 16:39:42 -0400

    create profile page to create accounts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,27 @@
-import '../../styles/globals.css';
+import "../../styles/globals.css";
 import "../../styles/mainWrapper.css";
 import "../../styles/mssgBoard.css";
 import "../../styles/sidebar.css";
 import "../../styles/catBox.css";
-import '../../configureAmplify'
+import "../../configureAmplify";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <Link href="/">
+          <span>Home</span>
+        </Link>
+        <Link href="/profile">
+          <span>Profile</span>
+        </Link>
+      </nav>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/pages/profile.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify'
+
+function Profile() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    checkUser();
+  }, []);
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    setUser(user)
+  }
+  if(!user) return null;
+  return (
+    <div>
+      <h1>Profile</h1>
+      <h3>{user.username}</h3>
+      <p>{user.attributes.email}</p>
+      <AmplifySignOut />
+    </div>
+  )
+}
+
+export default withAuthenticator(Profile)
