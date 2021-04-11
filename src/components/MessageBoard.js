79f7ac2bb6ce691cd8332b0a028468d96494fc30
JD Martinez <focus4ursoul@gmail.com>
2021-04-10 19:42:45 -0700

    clear messages when state mssgs are clear

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -4,53 +4,61 @@ import { useSmoomsContext } from "../utils/SmoomsState";
 
 // this component is the container for displaying all individual messages
 export default function Messages({ messages }) {
-  const [{ activeTags, activeMssgs }, dispatch] = useSmoomsContext();
+    const [{ activeTags, activeMssgs }, dispatch] = useSmoomsContext();
 
-  useEffect(() => {
-    if (activeTags.length > 0) {
-      let newMssgs = [];
-      let activeTagIDs = {};
+    console.log(activeMssgs);
 
-      messages.forEach((mssg) => {
-        let count = 0;
-        let tagsArr = mssg.tags.items;
+    useEffect(() => {
+        if (activeTags.length > 0) {
+            let newMssgs = [];
+            let activeTagIDs = {};
 
-        activeTags.forEach((aTag) => {
-          activeTagIDs[aTag] = true;
-        });
+            messages.forEach((mssg) => {
+                let count = 0;
+                let tagsArr = mssg.tags.items;
 
-        tagsArr.forEach((obj) => {
-          if (activeTagIDs[obj.tagID]) {
-            count++;
-          }
-        });
+                activeTags.forEach((aTag) => {
+                    activeTagIDs[aTag] = true;
+                });
 
-        if (count === activeTags.length) {
-          newMssgs.push(mssg);
+                tagsArr.forEach((obj) => {
+                    if (activeTagIDs[obj.tagID]) {
+                        count++;
+                    }
+                });
+
+                if (count === activeTags.length) {
+                    newMssgs.push(mssg);
+                }
+
+                count = 0;
+                return;
+            });
+
+            dispatch({ type: "filterMssgs", payload: { messages: newMssgs } });
+        } else {
+            dispatch({ type: "clearMssgs" })
         }
 
-        count = 0;
-        return;
-      });
 
-      dispatch({ type: "filterMssgs", payload: { messages: newMssgs } });
-    }
-  }, [activeTags]);
+    }, [activeTags]);
 
-  const displayMessages =
-    activeMssgs.length > 0 ? (
-      activeMssgs.map((mssg) => {
-        return (
-          <Message
-            key={mssg.id}
-            body={mssg.body}
-            tagPayload={mssg.tags?.items}
-          />
-        );
-      })
-    ) : (
-      <div>No messages</div>
-    );
+    const displayMessages =
+        activeMssgs.length > 0 ? (
+            activeMssgs.map((mssg) => {
+                return (
+                    <Message
+                        key={mssg.id}
+                        body={mssg.body}
+                        tagPayload={mssg.tags?.items}
+                    />
+                );
+            })
+        ) : (
+                <div>👋 Hi! select tags to pull up messages</div>
+            );
 
-  return <section className="mssgBoardWrapper">{displayMessages}</section>;
+    return <section className="mssgBoardWrapper">
+        {displayMessages}
+    </section>;
 }
