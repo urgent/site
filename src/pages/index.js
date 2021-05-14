d032825815c3a2dc13000233900ae954640abc23
urgent <urgent.programmer@gmail.com>
2021-05-13 22:43:44 -0400

    #4 -- Auth in CreateMessageWrapper
    next-auth in CreateMessageWrapper, replaces amplify auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,7 @@
 import MainWrapper from "../components/MainWrapper";
-import { signIn, signOut, useSession } from 'next-auth/client'
 
 export default function Home() {
-  const [session, loading] = useSession()
-
   return (
-    <>
-      {!session && <>
-        Not signed in <br />
-        <button onClick={signIn}>Sign in</button>
-      </>}
-      {session && <>
-        Signed in as {session.user.name} <br />
-        <button onClick={signOut}>Sign out</button>
-      </>}
-      <MainWrapper className="mainWrapper" />
-    </>
+    <MainWrapper className="mainWrapper" />
   )
 }
