97414af7bc34c9d0083736054ac5d07b71ef0835
urgent <urgent.programmer@gmail.com>
2021-05-14 17:16:05 -0400

    #3 -- Removed Relay Environment
    Remove Hasura. Run GraphQL server on NextJS with relay-nextjs
    
    Too much customization to pass the jwt to the RelayEnvironment, and
    to run the loadQuery on initial app load.
    
    Used in favor of relay-nextjs, proven solution

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,6 @@
 import MainWrapper from "../components/MainWrapper";
-import { useSession } from 'next-auth/client'
-import { RelayEnvironmentProvider } from 'react-relay/hooks';
-import RelayEnvironment from '../RelayEnvironment';
 
 export default function Home() {
-
-  const [session] = useSession();
-
-  if (session) {
-    return (
-      <RelayEnvironmentProvider environment={RelayEnvironment(session.token)}>
-        <MainWrapper className="mainWrapper" />
-      </RelayEnvironmentProvider>
-    )
-  }
-
   return (
     <MainWrapper className="mainWrapper" />
   )
