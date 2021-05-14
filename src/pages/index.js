933e92853561c88ba0b9b34a863c4b1f72876bc0
urgent <urgent.programmer@gmail.com>
2021-05-14 12:09:37 -0400

    #3 -- Relay with JWT
    Curry Relay environment with JWT from next-auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,20 @@
 import MainWrapper from "../components/MainWrapper";
+import { useSession } from 'next-auth/client'
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import RelayEnvironment from '../RelayEnvironment';
 
 export default function Home() {
+
+  const [session] = useSession();
+
+  if (session) {
+    return (
+      <RelayEnvironmentProvider environment={RelayEnvironment(session.token)}>
+        <MainWrapper className="mainWrapper" />
+      </RelayEnvironmentProvider>
+    )
+  }
+
   return (
     <MainWrapper className="mainWrapper" />
   )
