d8a58650f226c393e78b1b2e6caaf594913a933b
urgent <urgent.programmer@gmail.com>
2021-05-14 10:34:43 -0400

    #11 -- Removed aws-exports
    Missing, needs outside distro, difficult to regenerate without amplify init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,6 @@ import "../../styles/mssgBoard.css";
 import "../../styles/sidebar.css";
 import "../../styles/catBox.css";
 import "../../styles/Nav.css";
-import "../../configureAmplify";
 import { SmoomsProvider } from "../utils/SmoomsState";
 import Link from "next/link";
 import { Provider } from 'next-auth/client'
