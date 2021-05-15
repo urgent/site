6c1f4d039e47ae4ce5dec164645d4e1435bb9a68
urgent <urgent.programmer@gmail.com>
2021-05-15 16:14:12 -0400

    Removed Amplify
    Open handles errors

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import Nav from "../components/Nav";
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { Auth } from 'aws-amplify'
 
 function Profile() {
   const [user, setUser] = useState(null);
@@ -9,8 +7,7 @@ function Profile() {
     checkUser();
   }, []);
   async function checkUser() {
-    const user = await Auth.currentAuthenticatedUser();
-    setUser(user)
+
   }
   if (!user) return null;
   return (
@@ -19,9 +16,8 @@ function Profile() {
       <h1>Profile</h1>
       <h3>{user.username}</h3>
       <p>{user.attributes.email}</p>
-      <AmplifySignOut />
     </div>
   )
 }
 
-export default withAuthenticator(Profile)
+export default Profile
