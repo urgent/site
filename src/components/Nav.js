e5b8c624a70c767f090d78559f20abe1b8604865
urgent <urgent.programmer@gmail.com>
2021-05-18 16:11:59 -0400

    #12 -- Nav
    Nav per https://smooms.io/

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,25 @@
+import { Grid, Box, Menu, MenuButton, Image } from '@chakra-ui/react'
 import Link from "next/link";
 
 export default function Nav() {
-
-
-
-    const clearTags = () => {
-
-    }
-
-
     return (
-        <nav className="navContainer">
-            <div className="navLeft">
-                <button className="sideBarItems" onClick={() => clearTags()} >Clear Tags</button>
-            </div>
+        <Grid
+            as="nav"
+            gridTemplateColumns="[navLeftMargin] 10px [logo] 48px [menu] auto [button] 48px [navRightMargin] 10px"
+            gridRow="nav"
+            gridColumn="sidebar / -1"
+            pt={4}
+        >
+            <Image width={12} src="/images/smooms.io.svg" alt="smooms.io" sx={{ gridColumn: "logo" }} />
+            <Box sx={{ gridColumn: "menu" }} mt={3} ml={3}>
+                <Menu>
+                    <MenuButton mx={2}>Product</MenuButton>
+                    <MenuButton mx={2}>Company</MenuButton>
+                    <MenuButton mx={2}>Blog</MenuButton>
+                    <MenuButton mx={2}>Contact</MenuButton>
+                </Menu>
+            </Box>
 
-            <div className="navRight">
-                <Link href="/">
-                    <span className="navItems">Home</span>
-                </Link>
-                <Link href="/profile">
-                    <span className="navItems">Profile</span>
-                </Link>
-            </div>
-        </nav>
+        </Grid>
     )
 }
