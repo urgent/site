7f6055225e4d160f0451dff6fd25b95165915222
urgent <urgent.programmer@gmail.com>
2021-05-19 15:00:42 -0400

    #14 -- Title, Menu
    Grid layout for category card with title and menu

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,35 @@
-import { Box } from "@chakra-ui/react"
+import { Grid, Box } from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons"
 
 export default function Category({ children }) {
     return (
-        <Box>
-            {children}
-        </Box>
+        <Grid
+            maxWidth={[16, 24, 36, 48, 48]}
+            minHeight={24}
+            mx="auto"
+            my={4}
+            borderRadius="10px"
+            fontSize={[8, 12, 12, 12, 12]}
+            textAlign="left"
+            boxShadow="4px 4px 15px 0 rgb(10 8 59 / 6%)"
+            gridTemplateRows="[titlebar] 16px [body] auto"
+            gridTemplateColumns="[content] auto [corner] 16px"
+        >
+            <Box
+                gridRow="titlebar"
+                gridColumn="corner"
+                pt={1}
+            >
+                <HamburgerIcon />
+            </Box>
+            <Box
+                gridRow="titlebar"
+                gridColumn="content"
+                pl={1}
+                pt={1}
+            >
+                {children}
+            </Box>
+        </Grid>
     )
 }
