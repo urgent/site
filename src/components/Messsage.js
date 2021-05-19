2e814e2747678925b3d8d332d41072b0ad146d57
urgent <urgent.programmer@gmail.com>
2021-05-19 12:35:20 -0400

    #14 -- Key for Messages
    Added key for Message in Tile, needs for map

--- src/components/Messsage.js
+++ src/components/Messsage.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Box } from "@chakra-ui/react"
 
 // this component displays an individual message
-export default function Messsage({ children }) {
+export default function Messsage({ key, children }) {
   return (
     <Box
+      key={key}
       boxShadow="4px 4px 15px 0 rgb(10 8 59 / 6%)"
       borderRadius="10px"
       px={4}
