dfaa75ca738b18eeeab09ff0f4c34b0fb993daa3
urgent <urgent.programmer@gmail.com>
2021-05-19 11:53:33 -0400

    #14 -- Message Styles
    Styles for message tiles

--- src/components/Messsage.js
+++ src/components/Messsage.js
@@ -4,6 +4,12 @@ import { Box } from "@chakra-ui/react"
 // this component displays an individual message
 export default function Messsage({ children }) {
   return (
-    <Box>{children}</Box>
+    <Box
+      boxShadow="4px 4px 15px 0 rgb(10 8 59 / 6%)"
+      borderRadius="10px"
+      px={4}
+      py={6}
+      textAlign="left"
+    >{children}</Box>
   );
 }
