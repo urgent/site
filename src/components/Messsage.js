276e3f5aa60267b7abb30ef4fa68f191c172c5e3
urgent <urgent.programmer@gmail.com>
2021-05-20 18:05:47 -0400

    #17 -- Toolbar with Grid
    Layout for edit/delete message buttons

--- src/components/Messsage.js
+++ src/components/Messsage.js
@@ -1,28 +1,37 @@
 import React from "react";
-import { Box } from "@chakra-ui/react"
+import Toolbar from "./Toolbar"
+import { Grid, Box } from "@chakra-ui/react"
 
 function display(visible) {
   if (visible) {
-    return (
-      <div>
-        "# ! X"
-      </div>
-    )
+    return <Toolbar />
   }
 }
 
 // this component displays an individual message
 export default function Messsage({ edit, children }) {
   return (
-    <Box
+    <Grid
       boxShadow="4px 4px 15px 0 rgb(10 8 59 / 6%)"
       borderRadius="10px"
-      px={4}
-      py={6}
       textAlign="left"
+      gridTemplateRows="[menu] 2em [body] auto"
+      gridTemplateColumns="[body] auto [menu] 4.5em"
     >
-      {children}
-      {display(edit)}
-    </Box>
+      <Box
+        gridRow="menu"
+        gridColumn="menu"
+      >
+        {display(edit)}
+      </Box>
+      <Box
+        gridRow="body"
+        gridColumn="body / -1"
+        px={4}
+        pb={2}
+      >
+        {children}
+      </Box>
+    </Grid>
   );
 }
