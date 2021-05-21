aab522492cc896af769cd0adca4e85918e7c17d0
urgent <urgent.programmer@gmail.com>
2021-05-20 21:56:24 -0400

    #17 -- Category toolbar
    Center category name for alignment

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,23 +1,37 @@
 import Tag from "../components/Tag"
-import { Grid, Box, Wrap, WrapItem } from "@chakra-ui/react"
+import Toolbar from "./Toolbar"
+import { Grid, HStack, Box, Wrap, WrapItem } from "@chakra-ui/react"
 import { HamburgerIcon } from "@chakra-ui/icons"
 
-export default function Category({ category }) {
+function display(visible) {
+    if (visible) {
+        return <Toolbar />
+    }
+}
+
+export default function Category({ edit, category }) {
     return (
         <Grid
             maxWidth={[16, 24, 36, 48, 48]}
             minHeight={24}
             mx="auto"
             my={4}
+            pb={4}
             borderRadius="10px"
             fontSize={[8, 12, 12, 12, 12]}
             textAlign="left"
             boxShadow="4px 4px 15px 0 rgb(10 8 59 / 6%)"
-            gridTemplateRows="[titlebar] 24px [body] auto"
-            gridTemplateColumns="[content] auto [corner] 24px"
+            gridTemplateRows="[toolbar] 2rem [titlebar] 2rem [body] auto"
+            gridTemplateColumns="[content] 4fr [corner] 1fr"
         >
             <Box
-                gridRow="titlebar"
+                gridRow="toolbar"
+                gridColumn="content"
+            >
+                {display(edit)}
+            </Box>
+            <Box
+                gridRow="toolbar"
                 gridColumn="corner"
                 pt={1}
             >
@@ -25,9 +39,10 @@ export default function Category({ category }) {
             </Box>
             <Box
                 gridRow="titlebar"
-                gridColumn="content"
+                gridColumn="content / -1"
                 pl={1}
                 pt={1}
+                textAlign="center"
             >
                 {category.name}
             </Box>
