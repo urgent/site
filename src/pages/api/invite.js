d6ed1da7648a14d2017045b403b292a8cc93a046
urgent <urgent.programmer@gmail.com>
2021-07-01 00:07:57 -0400

    #43 -- Insert invite on form submit

--- src/pages/api/invite.js
+++ src/pages/api/invite.js
@@ -7,8 +7,11 @@ export default async (req, res) => {
     } = req
     // add to invitation table
     const pool = new Pool()
-    const dbRes = await pool.query(`SELECT slug FROM organization WHERE slug=$1`, [req.id])
+    await pool.query(`SELECT set_config('user.id', 'server', false)`);
+    const dbRes = await pool.query(`INSERT INTO invite(organization_id, email) SELECT organization.id, $1 FROM organization WHERE slug=$2`, [email, slug])
     await pool.end()
     // create next-auth user via email provider
+
+
     res.status(200).json({ email, slug })
 }
