9422f8bebbe82679976562bdb62bc509959092ee
urgent <urgent.programmer@gmail.com>
2021-06-30 20:55:33 -0400

    #43 -- Form with endpoint, lookup slug from db

--- src/pages/api/invite.js
+++ src/pages/api/invite.js
@@ -1,17 +1,14 @@
-const { Pool, Client } = require('pg')
-// pools will use environment variables
-// for connection information
-const pool = new Pool()
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-})
-// you can also use async/await
-const res = await pool.query('SELECT NOW()')
-await pool.end()
-// clients will also use environment variables
-// for connection information
-const client = new Client()
-await client.connect()
-const res = await client.query('SELECT NOW()')
-await client.end()
+import { Pool } from 'pg';
+
+export default async (req, res) => {
+    const {
+        query: { email, slug },
+        method,
+    } = req
+    // add to invitation table
+    const pool = new Pool()
+    const dbRes = await pool.query(`SELECT slug FROM organization WHERE slug=$1`, [req.id])
+    await pool.end()
+    // create next-auth user via email provider
+    res.status(200).json({ email, slug })
+}
--- src/pages/invite/[id].js
+++ src/pages/invite/[id].js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Pool } from 'pg';
 
-function Page({ data }) {
-  console.log(data)
+function Page({ slug }) {
   return (
     <div style={{ display: "block", }}>
-      <form action="/api/">
+      <form action="/api/invite">
         <div style={{ display: "flex", margin: "10px" }}>
           <label style={{ margin: "10px" }}>Email</label>
           <input type="email" name="email" style={{ margin: "10px", border: "1px solid #CCC", padding: "4px 12px" }} />
+          <input type="hidden" name="slug" value={slug} />
+          <input type="submit" />
         </div>
       </form>
     </div>
@@ -19,10 +20,11 @@ function Page({ data }) {
 export async function getServerSideProps({ req, res, params }) {
   // Lookup ID
   const pool = new Pool()
-  const dbRes = await pool.query(`SELECT id FROM organization WHERE slug=$1`, [params.id])
+  const dbRes = await pool.query(`SELECT slug FROM organization WHERE slug=$1`, [params.id])
   await pool.end()
+  //dbRes.rows[0]?.slug
   // Pass data to the page via props
-  return { props: { data: JSON.stringify(dbRes.rows) } }
+  return { props: { slug: 1 } }
 }
 
 export default Page
