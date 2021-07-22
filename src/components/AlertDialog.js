a1fce0db31d0abbb14e4336b257ee3780026b0f4
urgent <urgent.programmer@gmail.com>
2021-07-21 20:34:13 -0400

    #57 -- Delete component
    Missing from last commit

---
+++ src/components/AlertDialog.js
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from "react"
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    Button
+} from "@chakra-ui/react"
+
+export default function ({ title, body, click, isOpen, setIsOpen }) {
+    const onClose = () => setIsOpen(false)
+    const cancelRef = React.useRef()
+
+    return (
+        <>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            {title}
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            {body}
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="solid" color="white" backgroundColor="primary.500" _hover={{ background: "hover.500" }} onClick={() => { click(); setIsOpen(false); }} ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    )
+}
