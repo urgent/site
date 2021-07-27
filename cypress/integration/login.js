7baa5f9c16cb92e70311b369dff20cdb512d6f5b
urgent <urgent.programmer@gmail.com>
2021-07-26 21:45:13 -0400

    #6 -- Login test

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -39,11 +39,6 @@ describe('Login page', () => {
                     Cypress.Cookies.defaults({
                         preserve: cookieName,
                     })
-
-                    // remove the two lines below if you need to stay logged in
-                    // for your remaining tests
-                    cy.visit('/api/auth/signout')
-                    cy.get('form').submit()
                 }
             })
     })
