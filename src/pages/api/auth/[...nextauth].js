70b9b91e2ad3ee2b16f5cff7317ebccaa8a286df
urgent <urgent.programmer@gmail.com>
2021-08-06 18:00:35 -0400

    #65 -- sync invite and org perms on login
    Invites at first supported new user creation. Invite a new user by email. Gain access
    to organization. Later, needed support to add existing users to organization.
    
    Copied hook from createUser next auth event to signIn event. Keep same workflow of requiring
    invited user to sign in before invite becomes active. Better than having users add permissions
    directly.
    
    Change default organization to newly invite, to show user he has new access.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -40,5 +40,26 @@ export default NextAuth({
             await pool.query(`SELECT create_organization($1,$2)`, [message.id, slug])
             await pool.end()
         },
+        async signIn(message) {
+            const pool = new Pool()
+            await pool.query(`SELECT set_config('user.id', 'server', false)`);
+            // for invites to an already existing user
+            // is email auth provider?
+            if (message.user.email) {
+                // look up any invites missing org permissions
+                const organizationRes = await pool.query(`INSERT INTO organization_user(organization_id, user_id) SELECT organization_id, u.id
+                FROM  invite i LEFT JOIN users u ON u.email = i.email
+                WHERE  NOT EXISTS (
+                   SELECT organization_id
+                   FROM organization_user
+                   WHERE  user_id = u.id AND organization_id = i.organization_id
+                   ) AND i.email=$1 RETURNING *`, [message.user.email]);
+                if (organizationRes.rows.length > 0) {
+                    // Set new organization as default, to show user invite has occurred
+                    await pool.query(`DELETE FROM user_config WHERE user_id=$1`, [organizationRes.rows[0]['user_id']])
+                    await pool.query(`INSERT INTO public.user_config(default_organization, user_id) VALUES($1,$2)`, [organizationRes.rows[0]['organization_id'], organizationRes.rows[0]['user_id']]);
+                }
+            }
+        }
     }
 })
