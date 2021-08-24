776dd973feaa812698e3d30ac1e208e9c7ae0f79
urgent <urgent.programmer@gmail.com>
2021-08-23 21:55:22 -0400

    #74 -- Inter font
    https://chakra-ui.com/guides/using-fonts#option-1-install-with-npm
    https://nextjs.org/docs/basic-features/font-optimization

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const config = {
+    fonts: {
+        heading: "Inter",
+        body: "Inter",
+    },
     initialColorMode: "light",
     useSystemColorMode: false,
     colors: {
