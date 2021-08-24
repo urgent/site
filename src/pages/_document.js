d5c53615f921632b05e7229a901b4c6522d44761
urgent <urgent.programmer@gmail.com>
2021-08-23 22:04:55 -0400

    #75 -- favicon
    Copied from smooms.io main site

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,6 +26,7 @@ class MyDocument extends Document {
                     <relayDocument.Script />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="/images/favicon.png" />
                 </Head>
                 <body>
                     <Main />
