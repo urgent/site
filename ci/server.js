6eba1667681274b32c7c7815f3533da42eb4b433
urgent <urgent.programmer@gmail.com>
2021-11-08 10:18:55 -0500

    #105 -- AWS logs, CI webhook endpoints
    Log to CloudWatch, for metrics and alarms
    
    Start of express api for shared secret github and cloudwatch integration

---
+++ ci/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.use(function (req, res, next) {
+    if (req.params.token === process.env.SECRET) {
+        next();
+    } else {
+        res.status(400);
+        res.send('Error');
+    }
+})
+
+app.get('/deploy', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/cloudwatch', (req, res) => {
+    res.send('Hello World');
+});
+
+
+// blue up    
+// green down
+// green up
+// blue down
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
