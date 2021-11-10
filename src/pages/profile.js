7095696157a85730368d29fb4d9076ea9cc509f4
urgent <urgent.programmer@gmail.com>
2021-11-10 14:31:15 -0500

    Test Salesforce form
    Client request

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import Nav from "../components/Nav";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    checkUser();
-  }, []);
-  async function checkUser() {
-
-  }
-  if (!user) return null;
-  return (
-    <div>
-      <Nav />
-      <h1>Profile</h1>
-      <h3>{user.username}</h3>
-      <p>{user.attributes.email}</p>
+  return <>
+    <div style={{width:"50%", margin:"3rem auto"}}>
+    <form action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+    <input type="hidden" name="oid" value="00D5f000005WwfN" />
+    <input type="hidden" name="retURL" value="http://align.company" />
+    <label for="first_name">First Name</label><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br />
+    <label for="last_name">Last Name</label><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br />
+    <label for="email">Email</label><input  id="email" maxlength="80" name="email" size="20" type="text" /><br />
+    <label for="url">Website</label><input  id="url" maxlength="80" name="url" size="20" type="text" /><br />
+    <label for="company">Company</label><input  id="company" maxlength="40" name="company" size="20" type="text" /><br />
+    <label for="city">City</label><input  id="city" maxlength="40" name="city" size="20" type="text" /><br />
+    <label for="state">State Province</label><input  id="state" maxlength="20" name="state" size="20" type="text" /><br />
+    <input type="submit" name="submit" />
+    </form>
     </div>
-  )
+  </>
 }
 
 export default Profile
