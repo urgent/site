39d50a1dafa8740764fbad16eff91bbc38505cbd
urgent <urgent.programmer@gmail.com>
2021-11-10 17:27:06 -0500

    Fix client form

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 function Profile() {
   return <>
   <iframe src="https://codesandbox.io/embed/react-hook-form-js-forked-0qsn8?fontsize=14&hidenavigation=1&theme=dark"
-     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     style={{width:"100%", height:"500px", border:"0", borderRadius: "4px", overflow:"hidden"}}
      title="React Hook Form (JS) (forked)"
      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
