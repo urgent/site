481f08d0f80c317406e71d80e23c91e39997e125
urgent <urgent.programmer@gmail.com>
2021-11-10 16:21:26 -0500

    Update SalesForce form
    Per client

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -5,14 +5,14 @@ function Profile() {
     <div style={{width:"50%", margin:"3rem auto"}}>
     <form action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
     <input type="hidden" name="oid" value="00D5f000005WwfN" />
-    <input type="hidden" name="retURL" value="http://align.company" />
+    <input type="hidden" name="retURL" value="http://align.company/about.html" />
     <label for="first_name">First Name</label><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br />
     <label for="last_name">Last Name</label><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br />
     <label for="email">Email</label><input  id="email" maxlength="80" name="email" size="20" type="text" /><br />
-    <label for="url">Website</label><input  id="url" maxlength="80" name="url" size="20" type="text" /><br />
     <label for="company">Company</label><input  id="company" maxlength="40" name="company" size="20" type="text" /><br />
     <label for="city">City</label><input  id="city" maxlength="40" name="city" size="20" type="text" /><br />
-    <label for="state">State Province</label><input  id="state" maxlength="20" name="state" size="20" type="text" /><br />
+    <label for="state">State/Province</label><input  id="state" maxlength="20" name="state" size="20" type="text" /><br />
+    <label for="00N5f00000MT3ym">Number of Starting Align Seats:</label><input  id="00N5f00000MT3ym" name="00N5f00000MT3ym" size="20" type="text" /><br>
     <input type="submit" name="submit" />
     </form>
     </div>
