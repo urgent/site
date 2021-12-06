81d0f6f8d92f23b6f0a0d526e2471f742b621e07
urgent <urgent.programmer@gmail.com>
2021-12-05 22:02:30 -0500

    #109 -- Stripe npm
    Files to be added with last commit

---
+++ src/pages/api/checkout.js
@@ -0,0 +1,22 @@
+import { Stripe } from 'stripe';
+
+export default async function handler(req, res) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    try {
+        const session = await stripe.checkout.sessions.create({
+            mode: "subscription",
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price: process.env.STRIPE_PRICE_ID,
+                    quantity: 1
+                },
+            ],
+            success_url: "http://localhost:3000/success.html",
+            cancel_url: "http://localhost:3000/cancel.html",
+        });
+        res.status(200).json(session)
+    } catch (e) {
+        res.status(200).json(e)
+    }
+}
---
+++ src/pages/checkout.js
@@ -0,0 +1,45 @@
+import { NextPage } from 'next';
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Create a Checkout Session.
+    const checkoutSession = await fetchPostJSON(
+        '/api/checkout_sessions',
+        { amount: input.customDonation },
+    );
+
+    if ((checkoutSession).statusCode === 500) {
+        console.error((checkoutSession).message);
+        return;
+    }
+
+    // Redirect to Checkout.
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: checkoutSession.id,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
+};
+
+const DonatePage = () => {
+    return (
+        <Layout title="Donate with Checkout | Next.js + TypeScript Example">
+            <div className="page-container">
+                <h1>Donate with Checkout</h1>
+                <p>Donate to our project 💖</p>
+                <form onSubmit={handleSubmit}>
+                </form>
+            </div>
+        </Layout>
+    );
+};
+
+export default DonatePage;
