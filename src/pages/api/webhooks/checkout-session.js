ea8803dbc67298b47d7b0fb33ba6d8870c6a9c9d
urgent <urgent.programmer@gmail.com>
2022-01-06 22:20:47 -0500

    #113 -- Tests for checkout session

---
+++ src/pages/api/webhooks/checkout-session.js
@@ -0,0 +1,53 @@
+export default {
+    created: 1641439956,
+    data: {
+        "id": "cs_test_a1emyVRedOLrdAjV6vOhZhPjKpjHxips5joWCqOGJDSCGhh0J9dnVGEBQ0",
+        "object": "checkout.session",
+        "after_expiration": null,
+        "allow_promotion_codes": null,
+        "amount_subtotal": null,
+        "amount_total": 100,
+        "automatic_tax": {
+            "enabled": false,
+            "status": null
+        },
+        "billing_address_collection": null,
+        "cancel_url": "https://example.com/cancel",
+        "client_reference_id": null,
+        "consent": null,
+        "consent_collection": null,
+        "currency": null,
+        "customer": null,
+        "customer_details": null,
+        "customer_email": "stripe-webhook-test@align.app",
+        "expires_at": 1641513161,
+        "livemode": false,
+        "locale": null,
+        "metadata": {
+            user_id: "new",
+            seats: 1
+        },
+        "mode": "payment",
+        "payment_intent": "pi_3K6OXkBbbsSm7MWz0rMyna5X",
+        "payment_method_options": {},
+        "payment_method_types": [
+            "card"
+        ],
+        "payment_status": "unpaid",
+        "phone_number_collection": {
+            "enabled": false
+        },
+        "recovered_from": null,
+        "setup_intent": null,
+        "shipping": null,
+        "shipping_address_collection": null,
+        "shipping_options": [],
+        "shipping_rate": null,
+        "status": "expired",
+        "submit_type": null,
+        "subscription": null,
+        "success_url": "https://example.com/success",
+        "total_details": null,
+        "url": null
+    }
+}
---
+++ src/pages/api/webhooks/index.spec.js
@@ -0,0 +1,40 @@
+import checkoutSession from './checkout-session'
+import { pay } from './index'
+import { Pool } from 'pg';
+
+
+describe('webhook', () => {
+    let pool;
+    beforeAll(async () => {
+        pool = new Pool({
+            user: process.env.POSTGRES_USER,
+            host: 'localhost',
+            database: 'smooms',
+            password: process.env.POSTGRES_PASSWORD,
+            port: 5432,
+        });
+    });
+    afterAll(async (done) => {
+        await pool.end();
+        done();
+    })
+
+    test('insert database records with payment intent event', async (done) => {
+        // simulate user creation:
+        checkoutSession.data.metadata.user_id = 1;
+        // not in db
+        await pool.query(`DELETE FROM stripe WHERE email=$1`, [checkoutSession.data.customer_email]);
+        const emptyRes = await pool.query(`SELECT * FROM stripe WHERE email=$1`, [checkoutSession.data.customer_email]);
+        expect(emptyRes.rows.length).toEqual(0);
+        await pay(checkoutSession);
+        const payRes = await pool.query(`SELECT * FROM stripe WHERE email=$1`, [checkoutSession.data.customer_email]);
+        expect(new Date(payRes.rows[0].stripe_transaction_date)).toEqual(new Date(checkoutSession.created * 1000));
+        expect(payRes.rows[0].amount).toEqual(new Intl.NumberFormat(`en-US`, {
+            currency: `USD`,
+            style: 'currency',
+        }).format(checkoutSession.data.amount_total));
+        expect(payRes.rows[0].quantity).toEqual(checkoutSession.data.metadata.seats);
+        expect(payRes.rows[0].user_id).toEqual(checkoutSession.data.metadata.user_id);
+        done();
+    })
+});
