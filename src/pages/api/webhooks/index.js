4e007946b0ef9c253e3a9829bc79c94ef4c1a48d
urgent <urgent.programmer@gmail.com>
2022-01-08 19:08:41 -0500

    #113 -- env values for pg

--- src/pages/api/webhooks/index.js
+++ src/pages/api/webhooks/index.js
@@ -6,8 +6,8 @@ import { Pool } from 'pg';
 export async function pay(intent) {
     const pool = new Pool({
         user: process.env.POSTGRES_USER,
-        host: 'localhost',
-        database: 'smooms',
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
         password: process.env.POSTGRES_PASSWORD,
         port: 5432,
     });
