ef2e8dd3ed02a68b70d2d4188b430d7fe8c4e58d
urgent <urgent.programmer@gmail.com>
2022-01-15 23:49:52 -0500

    #116 -- Loom embed in message

---
+++ src/components/LoomEmbed.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { oembed } from "@loomhq/loom-embed";
+
+export default function ({ loomSharedUrl }) {
+    const [videoHTML, setVideoHTML] = useState("");
+    useEffect(() => {
+        async function setupEmbed() {
+            const { html } = await oembed(loomSharedUrl, { width: 400 });
+            setVideoHTML(html)
+        }
+        setupEmbed();
+    }, []);
+    return <div dangerouslySetInnerHTML={{ __html: videoHTML }}></div>
+}
