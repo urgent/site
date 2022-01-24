b7583e91bc6ba304502f33b9c62e9ff12a2df20f
urgent <urgent.programmer@gmail.com>
2022-01-24 18:19:55 -0500

    #121 -- :wrench: tags page, added TypeScript
    Missing files from last commit

---
+++ src/pages/[...tag].tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import Nav from "../components/Nav";
+import { Collapsable } from "../components/Sidebar";
+import Tiles from "../components/Tiles";
+import Mobile from "../components/Mobile";
+import { withRelay } from "relay-nextjs";
+import { graphql, usePreloadedQuery } from "react-relay/hooks";
+import { Grid, Box } from "@chakra-ui/react";
+import { getClientEnvironment } from "../lib/client_environment";
+
+const HomeQuery = graphql`
+  query pages_HomeQuery {
+    ...NavFragment_organization
+    ...NavFragment_organizationUsers
+    ...NavFragment_userConfig
+    ...useSidebarFragment_messages
+    ...TilesFragment_messages
+    ...useSidebarFragment
+    ...NavFragment_invite
+  }
+`;
+
+function Home({ preloadedQuery }) {
+  const query = usePreloadedQuery(HomeQuery, preloadedQuery);
+
+  return (
+    <>
+      <Grid
+        data-cy="grid"
+        templateColumns="[nav] 4rem [sidebar] 2fr [content] 7fr"
+        bg={"background.50"}
+        color={"text.600"}
+        minHeight="100vh"
+        d={["none", "none", "none", "grid", "grid"]}
+      >
+        <Nav query={query} />
+        <Box gridColumn="sidebar" maxHeight="99vh" overflowY="scroll">
+          <Collapsable query={query} />
+        </Box>
+        <Box
+          as="main"
+          gridColumn="content"
+          pt={2}
+          mx="auto"
+          sx={{ textAlign: "center" }}
+          width="100%"
+          maxHeight="99vh"
+          overflowY="scroll"
+        >
+          <Tiles query={query} />
+        </Box>
+      </Grid>
+      <Box d={["inherit", "inherit", "inherit", "none", "none"]}>
+        <Mobile query={query} />
+      </Box>
+    </>
+  );
+}
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+function Error() {
+  return <>Error</>;
+}
+
+interface NextCtx {
+  cookies: any;
+}
+
+export default withRelay(Home, HomeQuery, {
+  // This property is optional.
+  error: Error,
+  // Fallback to render while the page is loading.
+  // This property is optional.
+  fallback: <Loading />,
+  // Create a Relay environment on the client-side.
+  // Note: This function must always return the same value.
+  createClientEnvironment: () => getClientEnvironment(),
+  // Gets server side props for the page.
+  serverSideProps: async (ctx) => {
+    // This is an example of getting an auth token from the request context.
+    // If you don't need to authenticate users this can be removed and return an
+    // empty object instead.
+
+    return {
+      token: (ctx.req as unknown as NextCtx).cookies[process.env.COOKIE_NAME],
+    };
+  },
+  // Server-side props can be accessed as the second argument
+  // to this function.
+  createServerEnvironment: async (
+    ctx,
+    // The object returned from serverSideProps. If you don't need a token
+    // you can remove this argument.
+    { token }
+  ) => {
+    const { createServerEnvironment } = await import(
+      "../lib/server_environment"
+    );
+    return createServerEnvironment(token);
+  },
+});
+
+// needs to move in SSG, getServerSideProps
+/*if (!organization) {
+        // if user config exists, use as default organization. If not, use first row in organization query
+        if (userConfig.allUserConfigs?.edges[0]?.node.defaultOrganization > 0) {
+            focusOrganization(userConfig.allUserConfigs?.edges[0]?.node.defaultOrganization);
+        } else {
+            focusOrganization(organizationUsers.allOrganizationUsers?.edges[0]?.node?.organizationByOrganizationId.rowId);
+        }
+    }*/
---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import { Provider } from "next-auth/client";
+import Head from "next/head";
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import { getInitialPreloadedQuery, getRelayProps } from "relay-nextjs/app";
+import { getClientEnvironment } from "../lib/client_environment";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../components/theme";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import "../styles.css";
+
+const clientEnv = getClientEnvironment();
+const initialPreloadedQuery = getInitialPreloadedQuery({
+  createClientEnvironment: () => getClientEnvironment(),
+});
+
+function MyApp({ Component, pageProps }) {
+  const relayProps = getRelayProps(pageProps, initialPreloadedQuery);
+  const env = relayProps.preloadedQuery?.environment ?? clientEnv;
+
+  return (
+    <>
+      <Head>
+        <title>Align | Interface</title>
+      </Head>
+      <Provider session={pageProps.session}>
+        <RelayEnvironmentProvider environment={env}>
+          <ChakraProvider theme={theme}>
+            <DndProvider backend={HTML5Backend}>
+              <Component {...pageProps} {...relayProps} />
+            </DndProvider>
+          </ChakraProvider>
+        </RelayEnvironmentProvider>
+      </Provider>
+    </>
+  );
+}
+
+export default MyApp;
