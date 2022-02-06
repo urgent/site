6ae7814d7ef87e2171733c43515651a443a3f3a8
urgent <urgent.programmer@gmail.com>
2022-02-06 16:38:50 -0500

    #122 -- Tag filter AND, not OR
    Fix, needed multiple queries. Last query is to return
    a message type

--- db/init/03-router.sql
+++ db/init/03-router.sql
@@ -2,10 +2,25 @@ BEGIN;
 
 CREATE FUNCTION tile(organization_id INTEGER, tag_id INTEGER[])
 RETURNS setof public.message AS $$
-    SELECT message.*
+-- filter as much as possible across indexes
+with message_filter AS (
+    SELECT message.*, message_tag.tag_id
     FROM public.message
     INNER JOIN public.message_tag ON message_tag.message_id = message.id
-    INNER JOIN UNNEST($2) as tagInput(tag_id) ON message_tag.tag_id = tagInput.tag_id
-    WHERE message.organization_id = $1;
+    WHERE message.organization_id = $1
+    AND message_tag.tag_id = any($2)
+),
+-- aggregate to get unique array of tags
+message_flat AS (
+    SELECT message_filter.id, array_agg(message_filter.tag_id) AS message_tag_ids
+    FROM message_filter
+    GROUP BY (message_filter.id)
+),
+-- AND condition on tags, need all tags to match
+message_ids AS (
+    SELECT message_flat.id FROM message_flat WHERE message_tag_ids @> $2
+)
+-- get messages
+SELECT message.* FROM message INNER JOIN message_ids ON message.id = message_ids.id;
 $$ LANGUAGE sql STABLE;
 COMMIT;
