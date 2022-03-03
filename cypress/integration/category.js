0fa93292d4e0567d5a4572836cd07254dac053ff
urgent <urgent.programmer@gmail.com>
2022-03-03 17:27:08 -0500

    #127 -- :wrench: Green functional category create

--- cypress/integration/category.js
+++ cypress/integration/category.js
@@ -1,15 +1,21 @@
 describe('Category', () => {
     it('creates', () => {
         cy.visit('/')
+        cy.wait(2000)
         cy.get('[data-cy=category]').should('have.length', 0)
         cy.setCookie(Cypress.env('COOKIE_NAME'), Cypress.env('SESSION_TOKEN'));
         cy.visit('/')
         cy.get('[data-cy=edit_mode]').click()
         cy.get('[data-cy=create_category]').click()
+        cy.wait(4000)
+        cy.get('[data-cy=editor] .ProseMirror').click()
         cy.get('[data-cy=editor] .ProseMirror').type('cy test')
+        cy.wait(4000)
         cy.get('[data-cy=save]').click()
+        cy.wait(2000)
+        cy.url().should('contain', '/edit')
         cy.visit('/')
-        cy.get('[data-cy=category]').should('have.length', 1)
+        cy.get('[data-cy=category]').first().contains('cy test')
 
     })
 
