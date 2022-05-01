6ee2d3d115ab88e60860fc7b48effe29bef06a8e
urgent <urgent.programmer@gmail.com>
2022-05-01 17:50:16 -0400

    #134 -- :beetle: Remove autocomplete for create message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -166,50 +166,6 @@ export function CreateMessage({ query, connections }) {
     InsertMessageMutation
   ) as [boolean, (config?: any) => void];
 
-  function autoComplete({ editor, transaction }) {
-    if (!editor.options.editorProps.autoComplete) {
-      // disable until fully implemented, needs payment
-      return true;
-      editor.options.editorProps.autoComplete = true;
-      setTimeout(async () => {
-        editor.options.editorProps.autoComplete = false;
-        const text = editor.getText();
-        const response = await fetch("/api/nlp", {
-          method: "POST",
-          body: JSON.stringify({
-            text,
-          }),
-        });
-        const data = await response.json();
-        const nlp = JSON.parse(data?.text);
-        editor.commands.setContent({
-          type: "doc",
-          content: [
-            {
-              type: "paragraph",
-              content: [
-                {
-                  type: "text",
-                  text: nlp.data.text,
-                },
-              ],
-            },
-          ],
-        });
-      }, 4000);
-    }
-  }
-
-  // autosave and autocomplete
-  useEffect(() => {
-    if (editor) {
-      editor.on("update", autoComplete);
-      return () => {
-        editor.off("update", autoComplete);
-      };
-    }
-  }, [editor]);
-
   function create() {
     const content = JSON.stringify(editor?.getJSON());
     insertMessage({
